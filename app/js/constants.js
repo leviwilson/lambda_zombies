973b0ef9b1d20a1c4dbbf7742f954565f2ad412f
Levi Wilson <levi@leviwilson.com>
2015-10-07 20:10:19 -0400

    fix MESSAGE_URL

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,2 +1,2 @@
 var BASE_URL = "https://fb1eqoiqoh.execute-api.us-west-2.amazonaws.com/ZombieWorkshopStage/",
-    MESSAGES_ENDPOINT = "zombie/message";
+    MESSAGES_ENDPOINT = BASE_URL + "zombie/message";
